b6982176aa5ff5dfbe7f9deb07ff0943f447aa22
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-10-12 18:21:24 -0500

    finished writing the very first kernel! Confirmed the addresses of different sections by disassembling kernel.elf and looking at kernel.map files

--- kernel.c
+++ kernel.c
@@ -4,3 +4,28 @@ typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
 
+
+void *memset(void *buf, char c, size_t n){
+    uint8_t *p = (uint8_t*)buf;
+    while(n--){
+        *p++ = c;
+    }
+    return buf;
+}
+
+void kernel_main(void){
+    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+
+    for(;;);
+}
+
+__attribute__((section(".text.boot")))
+__attribute__((naked))
+void boot(void){
+    __asm__ __volatile__(
+        "mv sp, %[stack_top]\n" //set the stack pointer
+        "j kernel_main\n"       //jump to the kernel main function
+        :
+        : [stack_top] "r" (__stack_top) // Pass the stack top address as %[stack_top]
+    );
+}
