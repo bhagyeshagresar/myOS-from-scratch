8b28ba97ed2eafd66a3469883c4e671f02194c85
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-10-19 18:32:20 -0500

    finished and tested the kernel_panic function

--- kernel.c
+++ kernel.c
@@ -47,9 +47,11 @@ void kernel_main(void){
     // }
     //memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
-    printf("\n\nHello %s\n", "World!");
-    printf("1 + 2 = %d, %x\n", 1+2, 0x1234abcd);
+    //printf("\n\nHello %s\n", "World!");
+    //printf("1 + 2 = %d, %x\n", 1+2, 0x1234abcd);
 
+    PANIC("booted!\n");
+    printf("unreachable here!\n");
 
     //create an infinite idle loop
     for(;;){
--- kernel.h
+++ kernel.h
@@ -9,8 +9,12 @@ struct sbiret{
 };
 
 
-#define PANIC(fmt, ...)
-    do{
-        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);
-        while(1){}
+/*
+    __FILE__ and __LINE__ are standard C predefined macros and are handled by the C preprocessor phase of compilation
+    __VA_ARGS__ is a special identifier that is expanded by the C preprocessor to become all the arguments that are passed to the macro after the last named argument.
+*/
+#define PANIC(fmt, ...)         \
+    do{                            \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while(1){} \
     }while(0)
