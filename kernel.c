e71056c080f8a400acf975cdb5bb893c84dcf988
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-12-10 22:43:28 -0600

    implemented a round-robin scheduler

--- kernel.c
+++ kernel.c
@@ -10,33 +10,35 @@ extern char __free_ram[], __free_ram_end[];
 struct process procs[PROCS_MAX]; // All process control structures.
 struct process *proc_a;
 struct process *proc_b;
+struct process *current_proc; //currently running process
+struct process *idle_proc;  //idle process
 
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
     long arg5, long fid, long eid){
-register long a0 __asm__("a0") = arg0; 
-register long a1 __asm__("a1") = arg1;
-register long a2 __asm__("a2") = arg2;
-register long a3 __asm__("a3") = arg3;
-register long a4 __asm__("a4") = arg4;
-register long a5 __asm__("a5") = arg5;
-register long a6 __asm__("a6") = fid;
-register long a7 __asm__("a7") = eid;
-    
-//according to SBI specification, a0 and a7 
-//a0 and a1 are with =r constraint because a0 and a1 are output operands(registers)      
-__asm__ __volatile__("ecall"
-        : "=r"(a0), "=r"(a1)
-        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
-        "r"(a6), "r"(a7)
-        : "memory");
+    register long a0 __asm__("a0") = arg0; 
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+        
+    //according to SBI specification, a0 and a7 
+    //a0 and a1 are with =r constraint because a0 and a1 are output operands(registers)      
+    __asm__ __volatile__("ecall"
+            : "=r"(a0), "=r"(a1)
+            : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+            "r"(a6), "r"(a7)
+            : "memory");
 
-return (struct sbiret){.error = a0, .value = a1}; /*according to SBI specifications, the ecall returns a pair of values in a0 and a1, a0 returning an error code*/
+    return (struct sbiret){.error = a0, .value = a1}; /*according to SBI specifications, the ecall returns a pair of values in a0 and a1, a0 returning an error code*/
 }
 
 
 void putchar(char ch){
-sbi_call(ch, 0, 0, 0, 0, 0, 0, 1/* Console Putchar */);
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1/* Console Putchar */);
 }
 
 //delay function implements a busy wait to prevent the character output from becoming too fast, which would make your terminal unresponsive
@@ -177,13 +179,46 @@ paddr_t alloc_pages(uint32_t n)
 }
 
 
+
+void yield(void)
+{
+    //search for a runnable process
+    struct process *next = idle_proc;
+    for(int i = 0; i < PROCS_MAX; i++)
+    {
+        struct process *proc = &procs[(current_proc->pid + i)%PROCS_MAX];
+        //this check skips the idle process
+        if(proc->state == PROC_RUNNABLE && proc->pid > 0)
+        {
+            next = proc;
+            break;
+        }
+    }
+
+    // If there's no runnable process other than the current one, return and continue processing
+    if(next == current_proc)
+    {
+        return;
+    }
+
+     // Context switch
+     struct process *prev = current_proc;
+     current_proc = next;
+     switch_context(&prev->sp, &next->sp);
+
+
+}
+
+
 void proc_a_entry(void)
 {
     printf("Starting process A\n");
     while(1)
     {
         putchar('A');
-        switch_context(&proc_a->sp, &proc_b->sp);
+        // switch_context(&proc_a->sp, &proc_b->sp);
+        // delay(); //spend some time here before you output B
+        yield();
         delay();
     }
 }
@@ -194,11 +229,12 @@ void proc_b_entry(void)
     while(1)
     {
         putchar('B');
-        switch_context(&proc_b->sp, &proc_a->sp);
+        // switch_context(&proc_b->sp, &proc_a->sp);
+        // delay(); //spend some time here before you output A
+        yield();
         delay();
     }
 }
-
 
 
 //Kernel exception handler. Store the address of this function in stvec register
@@ -333,15 +369,29 @@ void kernel_main(void){
     */
     
     //chapter 10: test for creation of two processes
+    // memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    // WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    // proc_a = create_process((uint32_t) proc_a_entry);
+    // proc_b = create_process((uint32_t) proc_b_entry);
+    // proc_a_entry(); //start the first process and trigger context switch
+
+    // PANIC("unreachable here!");
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+
+    printf("\n\n");
+
     WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = 0; // idle
+    current_proc = idle_proc;
 
     proc_a = create_process((uint32_t) proc_a_entry);
     proc_b = create_process((uint32_t) proc_b_entry);
-    proc_a_entry(); //start the first process and trigger context switch
 
-    PANIC("unreachable here!");
-
+    yield();
+    PANIC("switched to idle process");
 
 }
 
