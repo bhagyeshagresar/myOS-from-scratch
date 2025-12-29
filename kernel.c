fef2de1e8df490573656353a574d84812b70e3c5
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-12-29 00:15:41 -0600

    vector table implementation working with ebreak

--- kernel.c
+++ kernel.c
@@ -416,99 +416,15 @@ void free(void *ptr)
 
 }
 
-// __attribute__((naked))
-// __attribute__((aligned(4)))
-// void kernel_entry(void) {
-//     __asm__ __volatile__(
-//         "csrw sscratch, sp\n"
-//         "addi sp, sp, -4 * 31\n"
-//         "sw ra,  4 * 0(sp)\n"
-//         "sw gp,  4 * 1(sp)\n"
-//         "sw tp,  4 * 2(sp)\n"
-//         "sw t0,  4 * 3(sp)\n"
-//         "sw t1,  4 * 4(sp)\n"
-//         "sw t2,  4 * 5(sp)\n"
-//         "sw t3,  4 * 6(sp)\n"
-//         "sw t4,  4 * 7(sp)\n"
-//         "sw t5,  4 * 8(sp)\n"
-//         "sw t6,  4 * 9(sp)\n"
-//         "sw a0,  4 * 10(sp)\n"
-//         "sw a1,  4 * 11(sp)\n"
-//         "sw a2,  4 * 12(sp)\n"
-//         "sw a3,  4 * 13(sp)\n"
-//         "sw a4,  4 * 14(sp)\n"
-//         "sw a5,  4 * 15(sp)\n"
-//         "sw a6,  4 * 16(sp)\n"
-//         "sw a7,  4 * 17(sp)\n"
-//         "sw s0,  4 * 18(sp)\n"
-//         "sw s1,  4 * 19(sp)\n"
-//         "sw s2,  4 * 20(sp)\n"
-//         "sw s3,  4 * 21(sp)\n"
-//         "sw s4,  4 * 22(sp)\n"
-//         "sw s5,  4 * 23(sp)\n"
-//         "sw s6,  4 * 24(sp)\n"
-//         "sw s7,  4 * 25(sp)\n"
-//         "sw s8,  4 * 26(sp)\n"
-//         "sw s9,  4 * 27(sp)\n"
-//         "sw s10, 4 * 28(sp)\n"
-//         "sw s11, 4 * 29(sp)\n"
-
-//         "csrr a0, sscratch\n"
-//         "sw a0, 4 * 30(sp)\n"
-
-//         "mv a0, sp\n"
-//         "call handle_exception_trap\n"
-
-//         "lw ra,  4 * 0(sp)\n"
-//         "lw gp,  4 * 1(sp)\n"
-//         "lw tp,  4 * 2(sp)\n"
-//         "lw t0,  4 * 3(sp)\n"
-//         "lw t1,  4 * 4(sp)\n"
-//         "lw t2,  4 * 5(sp)\n"
-//         "lw t3,  4 * 6(sp)\n"
-//         "lw t4,  4 * 7(sp)\n"
-//         "lw t5,  4 * 8(sp)\n"
-//         "lw t6,  4 * 9(sp)\n"
-//         "lw a0,  4 * 10(sp)\n"
-//         "lw a1,  4 * 11(sp)\n"
-//         "lw a2,  4 * 12(sp)\n"
-//         "lw a3,  4 * 13(sp)\n"
-//         "lw a4,  4 * 14(sp)\n"
-//         "lw a5,  4 * 15(sp)\n"
-//         "lw a6,  4 * 16(sp)\n"
-//         "lw a7,  4 * 17(sp)\n"
-//         "lw s0,  4 * 18(sp)\n"
-//         "lw s1,  4 * 19(sp)\n"
-//         "lw s2,  4 * 20(sp)\n"
-//         "lw s3,  4 * 21(sp)\n"
-//         "lw s4,  4 * 22(sp)\n"
-//         "lw s5,  4 * 23(sp)\n"
-//         "lw s6,  4 * 24(sp)\n"
-//         "lw s7,  4 * 25(sp)\n"
-//         "lw s8,  4 * 26(sp)\n"
-//         "lw s9,  4 * 27(sp)\n"
-//         "lw s10, 4 * 28(sp)\n"
-//         "lw s11, 4 * 29(sp)\n"
-//         "lw sp,  4 * 30(sp)\n"
-//         "sret\n"
-//     );
-// }
-
-
-//Kernel exception handler. Store the address of this function in stvec register
 __attribute__((naked))
 __attribute__((aligned(4)))
-void kernel_entry(void){
+void kernel_entry(void) {
     __asm__ __volatile__(
-
-        
-        "csrrw sp, sscratch, sp\n"      // Retrieve the kernel stack of the running process from sscratch. sp now points to kernel stack and not user stack
-                                        //sscratch holds the oiginal value of sp from the user stack
-        //"csrw sscratch, sp\n"       //temporarily save user mode stack pointer at the time of exception occurrence
-        "addi sp, sp, -4*31\n"      //move the stack pointer down by 4x31 = 124 bytes(allocation)
-        "sw ra, 4*0(sp)\n"          //store the value of ra at an offset of 0bytes from the address held in sp. do the same for all the
-        "sw gp,  4 * 1(sp)\n"       //these are the general purpose registers of the interrupted task and are stored in the newly created
-        "sw tp,  4 * 2(sp)\n"       //kernel stack
+        "csrw sscratch, sp\n"
+        "addi sp, sp, -4 * 31\n"
+        "sw ra,  4 * 0(sp)\n"
+        "sw gp,  4 * 1(sp)\n"
+        "sw tp,  4 * 2(sp)\n"
         "sw t0,  4 * 3(sp)\n"
         "sw t1,  4 * 4(sp)\n"
         "sw t2,  4 * 5(sp)\n"
@@ -537,16 +453,13 @@ void kernel_entry(void){
         "sw s10, 4 * 28(sp)\n"
         "sw s11, 4 * 29(sp)\n"
 
-        "csrr a0, sscratch\n"      //the user mode's stack poiinter at the point of exception is stored in a0
-        "sw a0, 4*30(sp)\n"        //store the user mode's stack pointer to the stack frame so now the whole context is saved, a0 may now hold a garbage value
-        
-        "addi a0, sp, 4 * 31\n"   //reset the kernel stack
-        "csrw sscratch, a0\n"       //restore the stack pointer value(sscratch had the user stack pointer)
+        "csrr a0, sscratch\n"
+        "sw a0, 4 * 30(sp)\n"
 
-        "mv a0, sp\n"              //move the curren address of the stack frame into a0, In the RISC-V calling convention, the first function argument must be placed in the a0 register
+        "mv a0, sp\n"
         "call handle_exception_trap\n"
-        
-        "lw ra,  4 * 0(sp)\n"       //after the handle_trap returns the kernel resumes the interrupted process, loads the trap frame back to the GPRs
+
+        "lw ra,  4 * 0(sp)\n"
         "lw gp,  4 * 1(sp)\n"
         "lw tp,  4 * 2(sp)\n"
         "lw t0,  4 * 3(sp)\n"
@@ -576,11 +489,98 @@ void kernel_entry(void){
         "lw s9,  4 * 27(sp)\n"
         "lw s10, 4 * 28(sp)\n"
         "lw s11, 4 * 29(sp)\n"
-        "lw sp,  4 * 30(sp)\n"  
-        "sret\n"                //call the sret instruction to resume execution from the point where the exception occurred.
-        
+        "lw sp,  4 * 30(sp)\n"
+        "sret\n"
     );
 }
+
+
+//Kernel exception handler. Store the address of this function in stvec register
+// __attribute__((naked))
+// __attribute__((aligned(4)))
+// void kernel_entry(void){
+//     __asm__ __volatile__(
+
+        
+//         "csrrw sp, sscratch, sp\n"      // Retrieve the kernel stack of the running process from sscratch. sp now points to kernel stack and not user stack
+//                                         //sscratch holds the oiginal value of sp from the user stack
+//         //"csrw sscratch, sp\n"       //temporarily save user mode stack pointer at the time of exception occurrence
+//         "addi sp, sp, -4*31\n"      //move the stack pointer down by 4x31 = 124 bytes(allocation)
+//         "sw ra, 4*0(sp)\n"          //store the value of ra at an offset of 0bytes from the address held in sp. do the same for all the
+//         "sw gp,  4 * 1(sp)\n"       //these are the general purpose registers of the interrupted task and are stored in the newly created
+//         "sw tp,  4 * 2(sp)\n"       //kernel stack
+//         "sw t0,  4 * 3(sp)\n"
+//         "sw t1,  4 * 4(sp)\n"
+//         "sw t2,  4 * 5(sp)\n"
+//         "sw t3,  4 * 6(sp)\n"
+//         "sw t4,  4 * 7(sp)\n"
+//         "sw t5,  4 * 8(sp)\n"
+//         "sw t6,  4 * 9(sp)\n"
+//         "sw a0,  4 * 10(sp)\n"
+//         "sw a1,  4 * 11(sp)\n"
+//         "sw a2,  4 * 12(sp)\n"
+//         "sw a3,  4 * 13(sp)\n"
+//         "sw a4,  4 * 14(sp)\n"
+//         "sw a5,  4 * 15(sp)\n"
+//         "sw a6,  4 * 16(sp)\n"
+//         "sw a7,  4 * 17(sp)\n"
+//         "sw s0,  4 * 18(sp)\n"
+//         "sw s1,  4 * 19(sp)\n"
+//         "sw s2,  4 * 20(sp)\n"
+//         "sw s3,  4 * 21(sp)\n"
+//         "sw s4,  4 * 22(sp)\n"
+//         "sw s5,  4 * 23(sp)\n"
+//         "sw s6,  4 * 24(sp)\n"
+//         "sw s7,  4 * 25(sp)\n"
+//         "sw s8,  4 * 26(sp)\n"
+//         "sw s9,  4 * 27(sp)\n"
+//         "sw s10, 4 * 28(sp)\n"
+//         "sw s11, 4 * 29(sp)\n"
+
+//         "csrr a0, sscratch\n"      //the user mode's stack poiinter at the point of exception is stored in a0
+//         "sw a0, 4*30(sp)\n"        //store the user mode's stack pointer to the stack frame so now the whole context is saved, a0 may now hold a garbage value
+        
+//         "addi a0, sp, 4 * 31\n"   //reset the kernel stack
+//         "csrw sscratch, a0\n"       //restore the stack pointer value(sscratch had the user stack pointer)
+
+//         "mv a0, sp\n"              //move the curren address of the stack frame into a0, In the RISC-V calling convention, the first function argument must be placed in the a0 register
+//         "call handle_exception_trap\n"
+        
+//         "lw ra,  4 * 0(sp)\n"       //after the handle_trap returns the kernel resumes the interrupted process, loads the trap frame back to the GPRs
+//         "lw gp,  4 * 1(sp)\n"
+//         "lw tp,  4 * 2(sp)\n"
+//         "lw t0,  4 * 3(sp)\n"
+//         "lw t1,  4 * 4(sp)\n"
+//         "lw t2,  4 * 5(sp)\n"
+//         "lw t3,  4 * 6(sp)\n"
+//         "lw t4,  4 * 7(sp)\n"
+//         "lw t5,  4 * 8(sp)\n"
+//         "lw t6,  4 * 9(sp)\n"
+//         "lw a0,  4 * 10(sp)\n"
+//         "lw a1,  4 * 11(sp)\n"
+//         "lw a2,  4 * 12(sp)\n"
+//         "lw a3,  4 * 13(sp)\n"
+//         "lw a4,  4 * 14(sp)\n"
+//         "lw a5,  4 * 15(sp)\n"
+//         "lw a6,  4 * 16(sp)\n"
+//         "lw a7,  4 * 17(sp)\n"
+//         "lw s0,  4 * 18(sp)\n"
+//         "lw s1,  4 * 19(sp)\n"
+//         "lw s2,  4 * 20(sp)\n"
+//         "lw s3,  4 * 21(sp)\n"
+//         "lw s4,  4 * 22(sp)\n"
+//         "lw s5,  4 * 23(sp)\n"
+//         "lw s6,  4 * 24(sp)\n"
+//         "lw s7,  4 * 25(sp)\n"
+//         "lw s8,  4 * 26(sp)\n"
+//         "lw s9,  4 * 27(sp)\n"
+//         "lw s10, 4 * 28(sp)\n"
+//         "lw s11, 4 * 29(sp)\n"
+//         "lw sp,  4 * 30(sp)\n"  
+//         "sret\n"                //call the sret instruction to resume execution from the point where the exception occurred.
+        
+//     );
+// }
 
 //Handle the exception
 void handle_exception_trap(struct trap_frame *f){
@@ -606,7 +606,7 @@ void handle_timer_trap()
 }
 
 
-
+//timer interrupt handler without yield
 __attribute__((naked))
 __attribute__((aligned(4)))
 void timer_interrupt_handler(void) {
@@ -775,10 +775,16 @@ void timer_interrupt_handler(void) {
 
 // The Program Counter(PC) will jump to base address+offset based on Table 32 of RISC-V ISA. 
 // Refer 12.1.2. Supervisor Trap Vector Base Address (stvec) Register in RISC-V Privileged ISA
+//__attribute__((naked))
+//__attribute__((aligned(4)))
 void vector_table()
 {
     __asm__ __volatile__(
         "j kernel_entry\n"          // Excption handle stored at Base Address + 0
+        "j kernel_entry\n" 
+        "j kernel_entry\n" 
+        "j kernel_entry\n" 
+        "j kernel_entry\n" 
         "j timer_interrupt_handler\n"     // Timer Interrupt Hnadler stored at Base Address + 0x14 (refer Table 32 RISC-V Privileged ISA, the exceptioon code = 5 for timer interrupt)
     );
 }
@@ -792,12 +798,12 @@ void configure_trap_handling(bool trap_mode)
     if(trap_mode)
     {
         // vectored mode: BASE = vector_table, MODE = 1
-        base_address = ((uint32_t)vector_table & ~0x03) | 0x01;
+        base_address = (((uint32_t)vector_table & ~0x03) | 0x01);
     }
     else
     {
           // direct mode: BASE = kernel_entry, MODE = 0
-          base_address = ((uint32_t)kernel_entry & ~0x03) | 0x00;
+          base_address = (((uint32_t)kernel_entry & ~0x03) | 0x00);
     }
 
     WRITE_CSR(stvec, (uint32_t) base_address);
@@ -835,21 +841,29 @@ void kernel_main(void){
 
     
     //configure trap handling in vector mode since we are dealing with exceptions and interrupts both
-    //configure_trap_handling(true);
-    WRITE_CSR(stvec, (uint32_t)timer_interrupt_handler);
+    configure_trap_handling(true);
+    //WRITE_CSR(stvec, (uint32_t)timer_interrupt_handler);
 
     //Enable sstatus.SIE bit
     enable_supervisor_interrupt();
 
     //Enable the timer interrupt sie.STIE
     enable_timer_interrupt();
+    //__asm__ __volatile__("unimp"); 
+    __asm__ __volatile__("ebreak");
 
     //initialise the timer interrupt for the first time
     write_to_stimecmp(read_rtc() + 1000000);
+   
 
     // idle_proc = create_process(NULL);
     // idle_proc->pid = 0; // idle
     // current_proc = idle_proc;
+
+    // while(1)
+    // {
+    //     printf("Entered Main Thread\n");
+    // }
 
     // proc_a = create_process(&proc_a_entry);
     // proc_b = create_process(&proc_b_entry);
